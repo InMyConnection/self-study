73984020a1cad5f990fd44877014eef91dc17661
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-18 09:57:12 +0300

    digitDegree

---
+++ CodeSignal/CodeSignal_Arcade_041.cs
@@ -0,0 +1,26 @@
+/*Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we get to a one digit number.
+
+Given an integer, find its digit degree.
+
+Example
+
+For n = 5, the output should be
+digitDegree(n) = 0;
+For n = 100, the output should be
+digitDegree(n) = 1.
+1 + 0 + 0 = 1.
+For n = 91, the output should be
+digitDegree(n) = 2.
+9 + 1 = 10 -> 1 + 0 = 1.*/
+
+int digitDegree(int n) {
+char[] number = n.ToString().ToCharArray();
+int count = 0;
+while(number.Length > 1){
+    IEnumerable<int> query = number.Select(c => Int32.Parse(c.ToString()));
+    n = query.Sum();
+    count++;
+    number = n.ToString().ToCharArray();
+}
+return count;
+}
