a9b7d8837e9cf30867d062273fd35958f61d1993
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-18 12:09:21 +0300

    All Longest Strings

--- CodeSignal/CodeSignal_Arcade_009.cs
+++ CodeSignal/CodeSignal_Arcade_009.cs
@@ -6,14 +6,5 @@ For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
 allLongestStrings(inputArray) = ["aba", "vcd", "aba"].*/
 
 string[] allLongestStrings(string[] inputArray) {
-List<int> length = new List<int>();
-foreach (string s in inputArray)
-length.Add(s.Length);
-List<string> result = new List<string>();
-foreach (string s in inputArray){
-    if (s.Length == length.Max())
-    result.Add(s);
-}
-string[] outputArray = result.ToArray();
-return outputArray;
+return inputArray.Where(s => s.Length == inputArray.Max(str => str.Length)).ToArray();
 }
