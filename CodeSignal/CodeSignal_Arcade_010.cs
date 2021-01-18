503a6295cb2ed34f29e5d587dfb2d278a8c9077b
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-18 13:22:24 +0300

    commonCharacterCount

--- CodeSignal/CodeSignal_Arcade_010.cs
+++ CodeSignal/CodeSignal_Arcade_010.cs
@@ -8,27 +8,5 @@ commonCharacterCount(s1, s2) = 3.
 Strings have 3 common characters - 2 "a"s and 1 "c".*/
 
 int commonCharacterCount(string s1, string s2) {
-int result = 0;
-int res1;
-int res2;
-List<char> commonChars = new List<char>();
-foreach (char c1 in s1)
-foreach (char c2 in s2)
-if (c1 == c2)
-if (!commonChars.Contains(c1))
-commonChars.Add(c1);
-foreach (char c in commonChars){
-res1 = 0;
-res2 = 0;
-foreach (char c1 in s1)
-if (c == c1)
-res1++;
-foreach (char c2 in s2)
-if (c == c2)
-res2++;
-if (res1 < res2)
-result += res1;
-else result += res2;
-}
-return result;
+return s1.Distinct().Sum(c => Math.Min(s1.Count(s => s == c), s2.Count(s => s == c)));
 }
