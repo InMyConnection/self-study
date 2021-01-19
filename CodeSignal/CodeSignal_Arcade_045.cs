498b216bdff59b1ff21f988c5d74d70ae73d515b
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-19 11:27:10 +0300

    buildPalindrome

--- CodeSignal/CodeSignal_Arcade_045.cs
+++ CodeSignal/CodeSignal_Arcade_045.cs
@@ -6,21 +6,9 @@ For st = "abcdc", the output should be
 buildPalindrome(st) = "abcdcba".*/
 
 string buildPalindrome(string st) {
-if (st.Equals(String.Concat(st.Reverse())))
+int length = st.Length;
+int count = 0;
+while (!st.Equals(String.Concat(st.Reverse())))
+st = st.Insert(length, st[count++].ToString());
 return st;
-string palindrome = String.Empty;
-List<string> output = new List<string>();
-for (int i = 0; i < st.Length; i++)
-{
-    string variant = String.Concat(st, String.Concat(st.Reverse()).Remove(0, i));
-    output.Add(variant);
-}
-output.Reverse();
-foreach (string variant in output)
-    if (variant.Equals(String.Concat(variant.Reverse())))
-    {
-        palindrome = variant;
-        break;
-    }   
-return palindrome;
 }
