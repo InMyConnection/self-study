55726f77f034e9e122f06b6787be5209f5c47135
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-20 08:13:22 +0300

    deleteDigit

--- CodeSignal/CodeSignal_Arcade_051.cs
+++ CodeSignal/CodeSignal_Arcade_051.cs
@@ -8,10 +8,5 @@ For n = 1001, the output should be
 deleteDigit(n) = 101.*/
 
 int deleteDigit(int n) {
-string number = n.ToString();
-int length = number.Length;
-int[] numbers = new int[length];
-for (int i = 0; i < length; i++)
-    numbers[i] = Int32.Parse(number.Remove(i, 1));
-return numbers.Max();
+return Int32.Parse(n.ToString().Select((c, i) => n.ToString().Remove(i, 1)).Max());
 }
