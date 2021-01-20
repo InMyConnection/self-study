798098910b0b24fc7d05d11bbe9215ccbdbb1836
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-20 12:12:12 +0300

    Sort by Height

--- CodeSignal/CodeSignal_Arcade_012.cs
+++ CodeSignal/CodeSignal_Arcade_012.cs
@@ -6,15 +6,7 @@ For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
 sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].*/
 
 int[] sortByHeight(int[] a) {
-List<int> result = new List<int>(a.Length);
-for (int i = 0; i < a.Length; i++){
-if (a[i] > 0)
-result.Add(a[i]);
-}
-result.Sort();
-for (int i = 0; i < a.Length; i++){
-if (a[i] < 0)
-result.Insert(i, a[i]);
-}
-return result.ToArray();
+int[] b = a.Where(n => n != -1).OrderBy(n => n).ToArray();
+int i = 0;
+return a.Select(n => n == -1 ? n : b[i++]).ToArray();
 }
