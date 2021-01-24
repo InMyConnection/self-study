e20e129b54abb885c0da3d588c895908fd6f8b70
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-24 12:20:32 +0300

    Add files via upload

---
+++ HeadFirstCsharp/HeadFirstCsharp02/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string result = "";
+            int x = 3;
+            if (x > 2)
+                result += "a";
+            x--;
+            result += "-";
+            if (x == 2)
+                result += "b c";
+            x--;
+            result += "-";
+            while (x > 0)
+            {
+                if (x == 1)
+                {
+                    result += "d";
+                    x--;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
