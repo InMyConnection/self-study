c209294ff232bb3ee0d8adb838b11e9b4a432791
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-24 12:22:36 +0300

    Add files via upload

---
+++ HeadFirstCsharp/HeadFirstCsharp02/Swimpool1/Swimpool1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Swimpool1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            string poem = "";
+            while (x < 4)
+            {
+                poem += "a";
+                if (x < 1)
+                    poem += " ";
+                poem += "n";
+                if (x < 1)
+                {
+                    poem += "oise ";
+                    x = x - 1;
+                }
+                if (x == 1)
+                    poem += "noys ";
+                if (x > 1)
+                    poem += " oyster";
+                x = x + 2;
+            }
+            Console.WriteLine(poem);
+        }
+    }
+}
