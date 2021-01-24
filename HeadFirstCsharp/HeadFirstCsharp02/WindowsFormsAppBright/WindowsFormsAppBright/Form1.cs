d1cea6082b2bec48744873adc9fed8f6861da87e
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-24 12:25:00 +0300

    Add files via upload

---
+++ HeadFirstCsharp/HeadFirstCsharp02/WindowsFormsAppBright/WindowsFormsAppBright/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppBright
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible)
+            {
+                int c = 0;
+                while (c < 254 && Visible)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                    c++;
+                }
+                while (c <= 0 && Visible)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                    c--;
+                }
+            }
+        }
+    }
+}
