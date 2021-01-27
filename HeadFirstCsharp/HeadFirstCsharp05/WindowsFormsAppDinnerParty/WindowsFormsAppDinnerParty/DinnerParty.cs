844e077d67383da2b45c56a588ea786364a8901c
InMyConnection <71561132+InMyConnection@users.noreply.github.com>
2021-01-27 15:58:28 +0300

    Add files via upload

---
+++ HeadFirstCsharp/HeadFirstCsharp05/WindowsFormsAppDinnerParty/WindowsFormsAppDinnerParty/DinnerParty.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsAppDinnerParty
+{
+    class DinnerParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+        public bool HealthyOption { get; set; }
+        public DinnerParty(int numberOfPeople, bool healthyOption, bool fancyDecorations)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            HealthyOption = healthyOption;
+        }
+        private decimal CalculateCostOfBeveragesPerPerson()
+        {
+            if (HealthyOption)
+                return 5;
+            return 20;
+        }
+        private decimal CalculateCostOfDecoration()
+        {
+            if (FancyDecorations)
+                return 50 + 15 * NumberOfPeople;
+            return 30 + 7.5M * NumberOfPeople;
+        }
+        public decimal Cost
+        {
+            get
+            {
+                decimal totalCost = (CostOfFoodPerPerson + CalculateCostOfBeveragesPerPerson()) * NumberOfPeople + CalculateCostOfDecoration();
+                if (HealthyOption)
+                    totalCost *= 0.95M;
+                return totalCost;
+            }
+        }
+    }
+}
---
+++ HeadFirstCsharp/HeadFirstCsharp05/WindowsFormsAppFarmer/WindowsFormsAppFarmer/Farmer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsAppFarmer
+{
+    class Farmer
+    {
+        private int feedMultiplier;
+
+        private int numberOfCows;
+
+        public Farmer(int numberOfCows, int feedMultiplier)
+        {
+            this.feedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+        public int BagsOfFeed { get; private set; }
+        public int FeedMultiplier { get { return feedMultiplier; } }
+    }
+}
